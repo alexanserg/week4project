37cbb37f9e47eb41e70203a8d53692fd0e035431
Student <example@example.com>
2019-06-21 17:40:31 -0700

    add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Written by Alex Sergejev
+## Page to order Pizza
+
+#### Choose toppings and size to see price.
+##### Opens in browser
+##### written using JavaScript, JQuery, CSS, Bootstrap and HTML
+###### Copyright(c) 2019 by Alex Sergejev
+###### Licensed under the MIT license
--- js/scripts.js
+++ js/scripts.js
@@ -13,17 +13,20 @@ Pizza.prototype.big = function () {
 Pizza.prototype.price = function () {
   return this.size * 5;
 }
+
+
 var pizza = new Pizza();
 
 
 $(document).ready(function() {
   $("#pizza").submit(function(event) {
     event.preventDefault();
+    pizza.topping = [];
     var topping = ($("#topping").val());
     var size = (parseFloat($("input:radio[name=size]:checked").val()))
     pizza.big(size);
     pizza.add(topping);
-    $("#price").text("Your pizza with " + pizza.topping + " will be $" + pizza.price());
+    $("#price").text("Your pizza with " + pizza.topping + " will be $" + pizza.price().toFixed(2));
 
   })
 })
